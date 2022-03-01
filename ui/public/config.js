c89fed77f439bc77bd0301991028c8b4d724129b
Pete <polson@dev-10.com>
2022-03-01 13:19:56 -0600

    update login.js to use env var

--- ui/public/config.js
+++ ui/public/config.js
@@ -1,2 +1,2 @@
-window.USER_SERVICE_HOSTPORT = "localhost:8080";
-window.FACILITY_SERVICE_HOSTPORT = "localhost:8081"
+window.USER_SERVICE_HOSTNAME = "localhost";
+window.FACILITY_SERVICE_HOSTNAME = "localhost"
--- ui/src/components/accounts/Login.js
+++ ui/src/components/accounts/Login.js
@@ -2,68 +2,70 @@ import React, { useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import AuthContext from "../../context/AuthContext";
+import { USER_SERVICE_URL } from "../../services/urls";
 
 
 function Login({ userStatus }) {
 
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [errors, setErrors] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-const contextValue = useContext(AuthContext);
-const [_, setUserStatus] = useContext(AuthContext);
+  const [_, setUserStatus] = useContext(AuthContext);
 
-const history = useHistory();
+  const history = useHistory();
 
-const handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const body = JSON.stringify({username,
-      password,})
-      console.log(body)
+    const body = JSON.stringify({
+      username,
+      password,
+    })
+    console.log(body)
     //placeholder, double check to make sure this is the current fetch address
-    const response = await fetch("http://localhost:8080/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username,
-            password,
-        }),
+    const response = await fetch(`${USER_SERVICE_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
     });
-    
-    if(response.status === 200){
-        const { jwt } = await response.json();
 
-        localStorage.setItem("token", jwt);
-        setUserStatus({ user: jwtDecode(jwt) });
-        history.push("/");
-       ;
-    } else if(response.status === 400) {
-        const errors = await response.json();
-        setErrors(errors);
+    if (response.status === 200) {
+      const { jwt } = await response.json();
+
+      localStorage.setItem("token", jwt);
+      setUserStatus({ user: jwtDecode(jwt) });
+      history.push("/");
+      ;
+    } else if (response.status === 400) {
+      const errors = await response.json();
+      setErrors(errors);
     } else if (response.status === 403) {
-        setErrors(["Login failed."]);
+      setErrors(["Login failed."]);
     } else {
-        setErrors(["Unknown error."]);
+      setErrors(["Unknown error."]);
     }
-};
+  };
 
-const renderErrors = () => {
-  return errors.map( error => <li key={error}>{error}</li>)
-}
+  const renderErrors = () => {
+    return errors.map(error => <li key={error}>{error}</li>)
+  }
 
-    return ( 
-      <>
+  return (
+    <>
       {(errors.length > 0) && (
         <div className="alert alert-danger">
           <ul>
             {renderErrors()}
           </ul>
-          </div>
+        </div>
       )}
-        <div>
+      <div>
         <h2>Login</h2>
 
         <form onSubmit={handleSubmit}>
@@ -86,8 +88,8 @@ const renderErrors = () => {
           </div>
         </form>
       </div>
-      </>
-    );
+    </>
+  );
 }
 
 export default Login;
