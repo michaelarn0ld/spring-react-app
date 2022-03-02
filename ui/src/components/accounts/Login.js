57afebdbf81c61a5c50eb857d62168ba13fba8da
Pisith Soeun <pisithsoeun@icloud.com>
2022-03-02 15:26:39 -0600

    Update Login.js

--- ui/src/components/accounts/Login.js
+++ ui/src/components/accounts/Login.js
@@ -58,44 +58,30 @@ function Login() {
           />
           <div className={"card-img-overlay"}>
             <div className="card-title">
-              <div className="card-body"></div>
-            </div>
-          </div>
-        </div>
-        <div className={"card bg-dark text-white"}>
-          <img
-            src={
-              "https://images.unsplash.com/photo-1596357395217-80de13130e92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d2VpZ2h0JTIwcm9vbXxlbnwwfHwwfHw%3D&w=1000&q=80"
-            }
-            className={"card-img opacity-50"}
-            alt={"yoga girl"}
-          />
-          <div className={"card-img-overlay"}>
-            <div className="card-title">
               <div className="card-body">
                 {errors.length > 0 && (
-                  <div className="alert alert-danger">
-                    <ul>{renderErrors()}</ul>
-                  </div>
+                    <div className="alert alert-danger">
+                      <ul>{renderErrors()}</ul>
+                    </div>
                 )}
                 <form onSubmit={handleSubmit}>
                   <div className="form-group">
                     <label htmlFor="username">Username</label>
                     <input
-                      type="text"
-                      className="form-control"
-                      id="username"
-                      placeholder="Username"
-                      onChange={(event) => setUsername(event.target.value)}
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Username"
+                        onChange={(event) => setUsername(event.target.value)}
                     />
                     <br />
                     <label htmlFor="password">Password</label>
                     <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="********"
-                      onChange={(event) => setPassword(event.target.value)}
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="********"
+                        onChange={(event) => setPassword(event.target.value)}
                     />
                     <br />
                     <div>
@@ -108,32 +94,6 @@ function Login() {
               </div>
             </div>
           </div>
-        </div>
-        <div className={"card bg-dark text-white"}>
-          <img
-            src={
-              "https://www.familyfuntwincities.com/wp-content/uploads/2020/07/grove-aquatic-center.jpg"
-            }
-            className={"card-img opacity-50"}
-            alt={"yoga girl"}
-          />
-          <div className={"card-img-overlay"}>
-            <div className="card-title">
-              <div className="card-body"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={"card-group"}>
-        <div className={"card bg-dark text-white"}>
-          <img
-            src={
-              "https://images.squarespace-cdn.com/content/v1/54beb580e4b00cf9dcf08db8/1498259961131-L11YYWPW2B9I8712HR50/gym+wide+2017.png?format=750w"
-            }
-            className={"card-img opacity-50"}
-            alt={"yoga girl"}
-          />
-          <div className={"card-img-overlay"}></div>
         </div>
       </div>
     </>
