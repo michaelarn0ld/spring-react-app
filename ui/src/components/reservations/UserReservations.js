a884a56cf33940133767e64da3af65fc8f43fb36
Pete <polson@dev-10.com>
2022-03-03 14:47:21 -0600

    fixed typo from last commit

--- ui/src/components/reservations/UserReservations.js
+++ ui/src/components/reservations/UserReservations.js
@@ -8,33 +8,40 @@ function UserReservations() {
   const [userStatus, setUserStatus] = useContext(AuthContext);
 
   useEffect(() => {
-    userStatus && fetch(`${window.FACILITY_SERVICE_URL}/reservations/${userStatus.user.id}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setReservations(data)
-      })
-      .catch((errors) => console.log(errors));
+    userStatus &&
+      fetch(
+        `${window.FACILITY_SERVICE_URL}/reservations/${userStatus.user.id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setReservations(data);
+        })
+        .catch((errors) => console.log(errors));
   }, [userStatus]);
 
   const deleteReservation = (reservation) => {
-    fetch(`http://localhost:8081/reservations/${userStatus.user.id}/${reservation}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    })
+    fetch(
+      `http://localhost:8081/reservations/${userStatus.user.id}/${reservation}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    )
       .then((response) => {
         if (response.status === 204) {
           const filteredReservations = reservations.filter(
             (r) => r.id !== reservation
           );
           setReservations(filteredReservations);
-          <div className=""="alert alert-success" role="alert">
+          <div className="alert alert-success" role="alert">
             Reservation successfully deleted
           </div>;
         } else if (response.status === 404) {
@@ -49,30 +56,38 @@ function UserReservations() {
   };
 
   const renderReservations = () => {
-      return (
+    return (
       <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Reservation ID</th>
-              <th scope="col">Equipment ID</th>
-              <th scope="col">Start</th>
-              <th scope="col">End</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {reservations && reservations.map(r => (
-                <tr>
-                  <td>{r.id}</td>
-                  <td>{r.equipmentId}</td>
-                  <td>{r.startTime}</td>
-                  <td>{r.endTime}</td>
-                  <td><div className="btn btn-danger" onClick={() => deleteReservation(r.id)}>Delete</div></td>
-                </tr>
+        <thead>
+          <tr>
+            <th scope="col">Reservation ID</th>
+            <th scope="col">Equipment ID</th>
+            <th scope="col">Start</th>
+            <th scope="col">End</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservations &&
+            reservations.map((r) => (
+              <tr>
+                <td>{r.id}</td>
+                <td>{r.equipmentId}</td>
+                <td>{r.startTime}</td>
+                <td>{r.endTime}</td>
+                <td>
+                  <div
+                    className="btn btn-danger"
+                    onClick={() => deleteReservation(r.id)}
+                  >
+                    Delete
+                  </div>
+                </td>
+              </tr>
             ))}
-          </tbody>
+        </tbody>
       </table>
-      )
+    );
   };
 
   const renderErrors = () => {
